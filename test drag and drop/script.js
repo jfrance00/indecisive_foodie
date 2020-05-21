d80bbd0257cd97f2a8349d17e1928b10970a67d2
Arthurgit cinfig --global user.email arthurr.ie@gmail.com <arthurr.ie@gmail.com>
2020-05-22 00:37:52 +0300

    drag and drop working

--- test drag and drop/script.js
+++ test drag and drop/script.js
@@ -1,65 +1,63 @@
-var x = 0;
-var y = 0;
-var offsetX= 0;
-var  offsetY=0;
+var x = 0; // initial X when moving an element
+var y = 0;// initital Y when moving an element
+var offsetX= 0; //offset of the element when dragging
+var  offsetY=0;//offset of the element when dragging
 let elem =document.getElementById('British')
+let bucket = document.getElementById("bucket");
+let coordBucket = bucket.getBoundingClientRect(); // get bucket coordinates
+let r = "i know";
 var active=false;
-elem.addEventListener('mousedown',function(e){
+
+
+elem.addEventListener('mousedown',function(e){ //addition of the mouse click down listener
 
   e.preventDefault();
       let elem =document.getElementById('British');
-      x=e.clientX - offsetX;
-      y=e.clientY- offsetY;
-      active=true;
-
-
+      x=e.clientX - offsetX;// current x position of the element
+      y=e.clientY- offsetY;// current y position of the element
+      active=true;// this element is active in movement
 });
 
-elem.addEventListener("mousemove",function(e){
+elem.addEventListener("mousemove",function(e){  //addition of the mouse move listener
   e.preventDefault();
   if(active){
   let elem =document.getElementById('British');
-  elem.style.left=e.clientX-x +"px";
-  elem.style.top=e.clientY-y+"px";
-  offsetX =e.clientX;
-  offsetY= e.clientY;
+  elem.style.left=e.clientX-x +"px"; //new position of the element
+  elem.style.top=e.clientY-y+"px";//new position of the element
+  offsetX =elem.style.left.slice(0,-2);//new offset of the element
+  offsetY= elem.style.top.slice(0,-2);//new offset of the element
 }
-
-
-})
-elem.addEventListener("mouseup",function(e){
+});
+elem.addEventListener("mouseup",function(e){  //addition of the mouse click up listener
   e.preventDefault();
-  x=e.clientX;
-  y=e.clientY;
-  active=false;
+  x=e.clientX; //current position of the element
+  y=e.clientY;//current position of the element
+  active=false;//the element is diactivated for movment
+  let bool = checkIfInsideBucket(e.target.id) // check if the buckt and the element are overlapping
+});
 
 
-})
+function checkIfInsideBucket(id){
+
+  let tag = document.getElementById(id);
+  let coordTag = tag.getBoundingClientRect();  //get element coordingates
 
 
 
-//
-//
-//
-// var contextmenu = document.getElementById('British');
-// var initX, initY, mousePressX, mousePressY;
-//
-// contextmenu.addEventListener('mousedown', function(event) {
-//   console.log("ere");
-// 	initX = this.offsetLeft;
-// 	initY = this.offsetTop;
-// 	mousePressX = event.clientX;
-// 	mousePressY = event.clientY;
-//
-// 	this.addEventListener('mousemove', repositionElement, false);
-//
-// 	window.addEventListener('mouseup', function() {
-// 	  contextmenu.removeEventListener('mousemove', repositionElement, false);
-// 	}, false);
-//
-// }, false);
-//
-// function repositionElement(event) {
-// 	this.style.left = initX + event.clientX - mousePressX + 'px';
-// 	this.style.top = initY + event.clientY - mousePressY + 'px';
-// }
+    if(coordBucket["top"] <= coordTag["top"] && coordBucket["left"] <= coordTag["left"] && coordBucket["bottom"] >= coordTag["bottom"] && coordBucket["right"] >= coordTag["right"]){
+          //check if overlapping
+        console.log("tag inside");
+
+        return true;
+    }
+    else{
+      console.log("tag not inside");
+      return false;
+    };
+    console.log(coordTag);
+    console.log(coordBucket);
+
+  return false;
+
+
+}
